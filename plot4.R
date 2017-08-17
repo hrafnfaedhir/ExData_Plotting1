2503bf9de50ba56c1ef51caffd4a82ced123422e
hrafnfaedhir <cpatterson.geness@gmail.com>
2017-08-17 16:21:16 -0700

    Corrected Week 1 assignment

--- plot4.R
+++ plot4.R
@@ -1,3 +1,9 @@
+power0 <- read.table("household_power_consumption.txt", header = TRUE,sep=";",na.strings = "?")
+power0$DT <- paste(power0$Date,power0$Time,sep=" ")
+power0$DateTime <- strptime(power0$DT,"%d/%m/%Y %H:%M:%S")
+date1 <- as.POSIXlt("2007-02-01 00:00:00")
+date2 <- as.POSIXlt("2007-02-02 24:00:00")
+power <- power0[(power0$DateTime>=date1)&(power0$DateTime<=date2),]
 png(filename = "plot4.png",height = 480,width=480)
 par(mfrow=c(2,2))
 #plot1
